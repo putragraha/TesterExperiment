63e4f38afa134c80fbacb03e96765cf9fd600477
Putra Nugraha <putra.nugraha@dana.id>
2020-12-13 23:21:49 +0700

    Migrate to AndroidX + kotlin

--- app/src/androidTest/java/com/android/nsystem/testapps/LoginActivityTest.java
+++ app/src/androidTest/java/com/android/nsystem/testapps/LoginActivityTest.java
@@ -1,18 +1,18 @@
 package com.android.nsystem.testapps;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.rule.ActivityTestRule;
+import androidx.test.espresso.Espresso;
+import androidx.test.rule.ActivityTestRule;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.not;
 
 public class LoginActivityTest {
