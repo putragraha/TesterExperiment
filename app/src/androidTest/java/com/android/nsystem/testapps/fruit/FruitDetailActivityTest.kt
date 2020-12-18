bd2893f84cc89d4ed993ac2b558f3a38116a461a
Putra Nugraha <putra.nugraha@dana.id>
2020-12-18 21:04:35 +0700

    [mod-5][part-3]create assertions for espresso test

--- app/src/androidTest/java/com/android/nsystem/testapps/fruit/FruitDetailActivityTest.kt
+++ app/src/androidTest/java/com/android/nsystem/testapps/fruit/FruitDetailActivityTest.kt
@@ -3,6 +3,9 @@ package com.android.nsystem.testapps.fruit
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.LayoutAssertions.noEllipsizedText
+import androidx.test.espresso.assertion.LayoutAssertions.noOverlaps
+import androidx.test.espresso.assertion.PositionAssertions.isCompletelyAbove
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
@@ -20,51 +23,87 @@ import org.junit.Test
  */
 class FruitDetailActivityTest {
 
+    private val watermelon by lazy { "Watermelon" }
+
     @get:Rule
     val fruitListActivity = ActivityScenarioRule(FruitListActivity::class.java)
 
     @Before
     fun setUp() {
+        fruitListActivity.scenario
         Intents.init()
+        openDetailActivity()
     }
 
-    @Test
-    fun textView_shouldShow_fruitName_from_bundle() {
+    @Test // View Assertion
+    fun fruitImage_shouldShown() {
         // given
-        fruitListActivity.scenario
-        val watermelon = "Watermelon"
 
         // when
-        onView(withId(R.id.rv_fruit))
-            .perform(
-                RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
-                    hasDescendant(withText(watermelon)),
-                    click()
-                )
-            )
+
+        // then
+        onView(withId(R.id.aciv_fruit_image)).check(matches(isDisplayed()))
+    }
+
+    @Test // View Assertion
+    fun textView_shouldShow_fruitName_from_bundle() {
+        // given
+
+        // when
 
         // then
         onView(withId(R.id.actv_fruit_name)).check(matches(withText(watermelon)))
     }
 
+    @Test // View Assertion
+    fun textView_shouldNotEllipsized() {
+        // given
+
+        // when
+
+        // then
+        onView(withId(R.id.actv_fruit_name)).check(noEllipsizedText())
+    }
+
+    @Test // Layout Assertion
+    fun layout_noOverlaps() {
+        // given
+
+        // when
+
+        // then
+        onView(withId(R.id.cl_fruit_detail)).check(noOverlaps())
+    }
+
     @Test
     fun backPressed_should_redirectBackTo_fruitListActivity() {
         // given
-        fruitListActivity.scenario
-        val watermelon = "Watermelon"
-        onView(withId(R.id.rv_fruit))
-            .perform(
-                RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
-                    hasDescendant(withText(watermelon)),
-                    click()
-                )
-            )
 
         // when
         pressBack()
 
         // then
         onView(withId(R.id.fab_message)).check(matches(isDisplayed()))
+    }
+
+    @Test // Position Assertion
+    fun fruitImage_shouldAbove_fruitName() {
+        // given
+
+        // when
+
+        // then
+        onView(withId(R.id.aciv_fruit_image)).check(isCompletelyAbove(withId(R.id.actv_fruit_name)))
+    }
+
+    private fun openDetailActivity() {
+        onView(withId(R.id.rv_fruit))
+            .perform(
+                RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+                    hasDescendant(withText(watermelon)),
+                    click()
+                )
+            )
     }
 
     @After
