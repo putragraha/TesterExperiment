6a03ad3581dee776165747a01e9e015f123a4f70
Putra Nugraha <putra.nugraha@dana.id>
2020-12-19 19:30:28 +0700

    create FruitListPresenterTest.kt

---
+++ app/src/test/java/com/android/nsystem/testapps/fruit/FruitListPresenterTest.kt
@@ -0,0 +1,34 @@
+package com.android.nsystem.testapps.fruit
+
+import com.nhaarman.mockitokotlin2.any
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.runners.MockitoJUnitRunner
+
+/**
+ * @author Putra Nugraha (putra.nugraha@dana.id)
+ * @version FruitListPresenterTest, v 0.1 19/12/20 18.57 by Putra Nugraha
+ */
+@RunWith(MockitoJUnitRunner::class)
+class FruitListPresenterTest {
+
+    @InjectMocks
+    lateinit var presenter: FruitListPresenter
+
+    @Mock
+    lateinit var view: FruitListContract.View
+
+    @Test
+    fun getData_shouldCall_onSuccessGetData_inView() {
+        // given
+
+        // when
+        presenter.getData()
+
+        // then
+        verify(view).onSuccessGetData(any())
+    }
+}
