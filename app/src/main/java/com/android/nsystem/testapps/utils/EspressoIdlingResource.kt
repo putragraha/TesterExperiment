b6c4d9708ffa98a45bf6622a35e3aa9b54eea743
Putra Nugraha <putra.nugraha@dana.id>
2021-01-01 00:20:15 +0700

    add visiblefortesting annotation for EspressoIdlingResource

--- app/src/main/java/com/android/nsystem/testapps/utils/EspressoIdlingResource.kt
+++ app/src/main/java/com/android/nsystem/testapps/utils/EspressoIdlingResource.kt
@@ -1,11 +1,13 @@
 package com.android.nsystem.testapps.utils
 
+import androidx.annotation.VisibleForTesting
 import androidx.test.espresso.idling.CountingIdlingResource
 
 /**
  * @author Putra Nugraha (putra.nugraha@dana.id)
  * @version EspressoIdlingResource, v 0.1 20/12/20 12.34 by Putra Nugraha
  */
+@VisibleForTesting(otherwise = VisibleForTesting.NONE)
 object EspressoIdlingResource {
 
     private const val RESOURCE = "GLOBAL"
